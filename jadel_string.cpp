556d0c2c5bcd98d4c9c4a452717e8209d793ec8b
Jaakko Tähtinen <jaakko.tahtinen@gmail.com>
2022-11-09 03:21:49 +0200

    Removed destructor, improved constructors and operator overloads, created functions for converting values to strings

--- jadel_string.cpp
+++ jadel_string.cpp
@@ -1,59 +1,170 @@
 #include "jadel_string.h"
 #include <string.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <stdio.h>
+#include "jadel_memory.h"
 
 namespace jadel
-{
+{/**
     String::String(const char *content)
     {
+        if (!content)
+        {
+            this->content = NULL,
+            size = 0;
+            return;
+        }
         size_t len = strlen(content);
-        this->content = (char *)malloc(len + 1);
+        this->content = (char *)jadel::memoryReserve(len + 1);
         memmove(this->content, content, len);
         this->size = len;
         this->content[this->size] = 0;
     }
 
     String::String(const String& other)
+        : String(other.content)
+    { 
+    }
+
+    String::String()
+        : content(NULL)
+        , size(0)
     {
-        size_t len = strlen(other.content);
-        this->content = (char *)malloc(len + 1);
-        memmove(this->content, other.content, len);
-        this->size = len;
-        this->content[this->size] = 0;        
     }
 
     String::~String()
     {
-        //free(content);
-        //size = 0;
+        jadel::memoryFree(content);
+        content = NULL;
+        size = 0;
+    }
+*/
+    String::String()
+    {
+    }
+
+    String::String(const char* content)
+    {
+        init(this, content);
+    }
+
+    bool String::init(String *string, size_t length)
+    {
+        if (!string || length == 0)
+        {
+            return false;
+        }
+        if (length <= JADEL_SMALL_STRING_SIZE)
+        {
+            string->isSmallString = true;
+            string->smallString[length] = '\0';
+        }
+        else
+        {
+            string->isSmallString = false;
+            string->content = (char *)jadel::memoryReserve(length + 1);
+            string->content[length] = '\0';
+        }
+        string->size = length;
+        return true;
+    }
+
+    bool String::init(String *string, const char* content)
+    {
+        if (!string || !content || content[0] == '\0')
+        {
+            return false;
+        }
+        size_t len = strlen(content);
+        init(string, len);
+        if (string->isSmallString)
+        {
+            memmove(string->smallString, content, len);
+        }
+        else
+        {
+            memmove(string->content, content, len);
+        }
+        return true;
+    }
+
+    void String::free()
+    {
+        if (!this->isSmallString)
+        {
+            jadel::memoryFree(content);
+        }
+        content = NULL;
+        size = 0;
     }
 
     const char *String::c_str() const
     {
-        return content;
-    }
+        if (size == 0) return NULL;
+        const char* result;
+        if (this->isSmallString)
+        {
+            result = smallString;
+        }
+        else
+        {
+            result = content;
+        }
+        return result;    }
 
     char *String::c_str()
     {
-        return content;
+        if (size == 0) return NULL;
+        char* result;
+        if (this->isSmallString)
+        {
+            result = smallString;
+        }
+        else
+        {
+            result = content;
+        }
+        return result;
     }
 
     char String::operator[](size_t index) const
     {
-        char result = content[index];
+        char result;
+        if (this->isSmallString)
+        {
+            result = smallString[index];
+        }
+        else
+        {
+            result = content[index];
+        }
         return result;
+    }
+
+    String& String::operator=(const char* other)
+    {
+        String::init(this, other);
+        return *this;
     }
 
     bool String::operator==(const String &other) const
     {
-        bool result = *this == other.content;
+        bool result = *this == other.c_str();
         return result;
     }
+
     bool String::operator==(const char *other) const
     {
-        bool result = strncmp(this->content, other, this->size);
-        return result == 0;
+        bool result;
+        if (this->isSmallString)
+        {
+            result = strncmp(this->smallString, other, this->size) == 0;
+        }
+        else
+        { 
+            result = strncmp(this->content, other, this->size) == 0;
+        }
+        return result;
     }
 
     bool String::operator!=(const String &other) const
@@ -68,11 +179,24 @@ namespace jadel
         return result;
     }
 
-    void String::print() const
+    String String::operator+(const String& b) const
     {
-        for (size_t i = 0; i < size; ++i)
-        {
-            putchar(content[i]);
-        }
+        String result;
+        const char* aContent = this->c_str();
+        const char* bContent = b.c_str();
+        size_t aLen = strlen(aContent);
+        size_t bLen = strlen(bContent);
+        size_t length =  aLen + bLen;
+        String::init(&result, length);
+        char* resultContent = result.c_str();
+        memmove(resultContent, aContent, aLen);
+        memmove(resultContent + aLen, bContent, bLen);
+        return result;
+    }
+
+    String String::operator+(const char* other) const
+    {
+        String result = *this + String(other);
+        return result;
     }
 }
--- jadel_string.h
+++ jadel_string.h
@@ -1,25 +1,84 @@
 #pragma once
 
+#include <stdio.h>
 #include "jadel_defs.h"
 
+#define JADEL_SMALL_STRING_SIZE (50)
 namespace jadel
 {
     struct DECLSPEC String
     {
+        char smallString[JADEL_SMALL_STRING_SIZE + 1];
         char *content;
         size_t size;
+        bool isSmallString;
 
-        String(const char *content);
-        String(const String& other);
-        ~String();
-        const char* c_str() const;
-        char* c_str();
+        // String(const char *content);
+        // String(const String& other);
+        // String();
+        //~String();
+        String();
+        String(const char* content);
+        static bool init(String *string, size_t length);
+        static bool init(String *string, const char *content);
+        void free();
+        const char *c_str() const;
+        char *c_str();
         char operator[](size_t index) const;
-        bool operator==(const String& other) const;
-        bool operator==(const char* other) const;
-        bool operator!=(const String& other) const;
-        bool operator!=(const char* other) const;
-        void print() const;
+        String &operator=(const char *other);
+        bool operator==(const String &other) const;
+        bool operator==(const char *other) const;
+        bool operator!=(const String &other) const;
+        bool operator!=(const char *other) const;
+        String operator+(const String &other) const;
+        String operator+(const char* other) const;
     };
+    
+    static char stringBuffer[50];
+    
+    inline String toString(int val)
+    {
+        sprintf(stringBuffer, "%d", val);
+        // message("%s\n", stringBuffer);
+        String result;
+        String::init(&result, stringBuffer);
+        return result;
+    }
 
+    
+    inline String toString(size_t val)
+    {
+        sprintf(stringBuffer, "%zd", val);
+        // message("%s\n", stringBuffer);
+        String result;
+        String::init(&result, stringBuffer);
+        return result;
+    }
+
+    inline String toString(unsigned int val)
+    {
+        sprintf(stringBuffer, "%d", val);
+        // message("%s\n", stringBuffer);
+        String result;
+        String::init(&result, stringBuffer);
+        return result;
+    }
+
+    inline String toString(float val)
+    {
+        sprintf(stringBuffer, "%f", val);
+        // message("%s\n", stringBuffer);
+        String result;
+        String::init(&result, stringBuffer);
+        return result;
+    }
+
+    inline String toString(double val)
+    {
+        sprintf(stringBuffer, "%f", val);
+        // message("%s\n", stringBuffer);
+        String result;
+        String::init(&result, stringBuffer);
+        return result;
+    }
 }
