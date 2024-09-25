77c6d66d572c4de16286d07e31c7577f64f0635b
JTahtinen <jaakko.tahtinen@gmail.com>
2024-09-25 19:44:49 +0300

    Added a new constructor and dot product function

--- jadel_vec3.cpp
+++ jadel_vec3.cpp
@@ -4,16 +4,19 @@
 namespace jadel
 {
     Vec3::Vec3(float x, float y, float z)
-        : x(x)
-        , y(y)
-        , z(z)
+        : x(x), y(y), z(z)
     {
     }
     Vec3::Vec3()
         : Vec3(0, 0, 0)
     {
     }
-        
+
+    Vec3::Vec3(Vec2 vec, float z)
+        : Vec3(vec.x, vec.y, z)
+    {
+    }
+
     Vec3 Vec3::add(Vec3 v1) const
     {
         Vec3 result(x + v1.x, y + v1.y, z + v1.z);
@@ -41,6 +44,12 @@ namespace jadel
         return result;
     }
 
+    float Vec3::dot(Vec3 other) const
+    {
+        float result = this->x * other.x + this->y * other.y + this->z * other.z;
+        return result;
+    }
+
     float Vec3::length() const
     {
         float result = sqrtf(x * x + y * y + z * z);
@@ -53,12 +62,12 @@ namespace jadel
         float len = length();
         if (len == 0)
         {
-            result = Vec3(0, 0 ,0);
+            result = Vec3(0, 0, 0);
         }
         else
         {
             result = Vec3(x / len, y / len, z / len);
         }
-        return result;   
+        return result;
     }
 }
